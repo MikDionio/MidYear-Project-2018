24128d20461744c46db246d41173ed6f690bdbc3
Mikhael Jude M. Dionio <mikhaeldionio@gmail.com>
2018-06-20 21:45:43 +0800

    adds character model and movement

---
+++ Assets/PlayerAnimScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimScript : MonoBehaviour {
+
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        float forward = Input.GetAxis("Vertical") * 3.55f;
+        float right = Input.GetAxis("Horizontal") * 2.86f;
+
+        if (forward != 0 ||right != 0){
+            animator.SetBool("isMoving", true);
+            animator.SetFloat("Forward", forward);
+            animator.SetFloat("Right", right);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+        
+        
+	}
+}
